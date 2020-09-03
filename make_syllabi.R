0eea64e9e73995f47400a75cd4a7f75ce278fca4
Gabriel Geisler Mesevage <gabriel.mesevage@gmail.com>
2020-09-03 17:48:31 +0300

    got the toc added after much wrestling with rmarkdown render

--- make_syllabi.R
+++ make_syllabi.R
@@ -4,7 +4,8 @@ library(tufte)
 
 rmarkdown::render("syllabus_london_city_of_capital.Rmd", 
                   output_format = "tufte_html", 
-                  output_file = "index.html")
+                  output_file = "index.html", 
+                  output_options = list(toc = TRUE))
 
 rmarkdown::render("syllabus_london_city_of_capital.Rmd", 
                   output_file = "moodle_html_syllabus.html")
